1d2e6922ac699057ad60d519fa48ab605f273eed
Anthony Maudry <amaudry@gmail.com>
2017-09-25 20:26:55 +0200

    First step

--- INSADesignPattern/Program.cs
+++ INSADesignPattern/Program.cs
@@ -10,6 +10,21 @@ namespace INSADesignPattern
     {
         static void Main(string[] args)
         {
+            string line;
+            Console.WriteLine("");
+            Console.WriteLine("     __   __     __  ________  _____");
+            Console.WriteLine("    /  / /  |  /  / /  _____/ /  _  |");
+            Console.WriteLine("   /  / /   | /  / /  /____  /  /_| |");
+            Console.WriteLine("  /  / /    |/  / /____   / /  ___  |");
+            Console.WriteLine(" /  / /  /|    / _____/  / /  /   | |");
+            Console.WriteLine("/__/ /__/ |___/ /_______/ /__/    |_|");
+            Console.WriteLine("Desing Patterns - Anthony Maudry amaudry@gmail.com");
+            Console.WriteLine("Hello,");
+            Console.WriteLine("Write something (type 'exit' to exit the program.");
+            while ((line = Console.ReadLine()) != "exit")
+            {
+                Console.WriteLine(line);
+            }
         }
     }
 }
